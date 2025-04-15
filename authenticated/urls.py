7dfcb4b1244cad51beb0e11071c1a6da2504c75d
Mikael Picolo <mikael.picolo@laposte.net>
2025-04-15 11:34:24 +0200

    correction path users

--- authenticated/urls.py
+++ authenticated/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 
 urlpatterns = [
     path('users/register/', UserCreateView.as_view(), name='user-register'),
-    path('user/', UserListView.as_view(), name='user_list'),
+    path('users/', UserListView.as_view(), name='user_list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
